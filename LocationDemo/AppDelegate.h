683e7c94496e726eb5645b1c2cebd8f1f15e33ad
Monster <Monster@wangrongrongdeMacBook-Pro.local>
2017-03-01 14:15:47 +0800

    firstCommit

---
+++ LocationDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LocationDemo
+//
+//  Created by Monster on 2017/3/1.
+//  Copyright © 2017年 Monster. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
